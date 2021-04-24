3f191738cad5e296d43fff1fdeaf3f800ec775a2
VytautasKaz <vytautas.kazakevicius2@gmail.com>
2021-04-24 15:01:43 +0300

    initial commit

---
+++ database/migrations/2021_04_24_115246_create_grades_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('lecture_id');
+            $table->foreign('lecture_id')
+                ->references('id')->on('lectures')
+                ->onDelete('cascade')
+                ->onUpdate('restrict');
+            $table->unsignedBigInteger('student_id');
+            $table->foreign('student_id')
+                ->references('id')->on('students')
+                ->onDelete('cascade')
+                ->onUpdate('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grades');
+    }
+}
