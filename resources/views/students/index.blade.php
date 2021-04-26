d662b8810a2a01d3ceb5e4d5c16c3ed8cf4bf0e9
VytautasKaz <vytautas.kazakevicius2@gmail.com>
2021-04-26 12:30:41 +0300

    removed commented code

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,18 +1,6 @@
 @extends('layouts.app')
 @section('content')
     <div class="card-body container">
-        {{-- <div style="margin-bottom: 32px;" class="card-body">
-            <form class="form-inline" action="{{ route('student.index') }}" method="GET">
-                <select style="margin-right: 20px;" name="horse_id" id="" class="form-control">
-                    <option value="" selected disabled>Choose a horse to filter by</option>
-                    @foreach ($horses as $horse)
-                        <option value="{{ $horse->id }}" @if ($horse->id == app('request')->input('horse_id')) selected="selected" @endif>{{ $horse->name }}</option>
-                    @endforeach
-                </select>
-                <button style="margin-right: 10px;" type="submit" class="btn btn-primary">Submit</button>
-                <a class="btn btn-success" href={{ route('student.index') }}>Show All</a>
-            </form>
-        </div> --}}
         @if (session('status_success'))
             <p style="color: green"><b>{{ session('status_success') }}</b></p>
         @else
